bd46873109689ee6b471a5b783b621cbe8bc6d98
ramindu90 <rami.desilva@gmail.com>
2016-06-29 00:27:40 +0530

    Adding Initial Jon finder dashboard page

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -1,3 +1,57 @@
 /**
  * Created by Ramindu on 6/26/16.
  */
+
+// $(document).ready(function() {
+//     $(".nav-side-menu li").click(function() {
+//         $(".nav-side-menu li").removeClass("active");
+//         $(this).addClass("active");
+//     });
+// });
+
+$(document).ready(function() {
+    $(".nav-side-menu .sub-menu.collapse li").click(function() {
+
+        $(".nav-side-menu .sub-menu.collapse li").removeClass("active");
+        $(this).addClass("active");
+        //gives its own ul id
+        var menuContentId = $(this).closest("ul").attr("id");
+
+        var ulListOwner = "#" + menuContentId;
+        var mainListItems = $("li.menu-list-item");
+        console.log(mainListItems.length);
+
+        mainListItems.each(function(idx, li) {
+            var attr = $(li).attr('data-target');
+            if(ulListOwner === attr){
+                $(li).addClass("active");
+            } else {
+                $(li).removeClass("active");
+            }
+        });
+    });
+});
+
+$(document).ready(function() {
+    $(".nav-side-menu li.menu-list-item").click(function() {
+        $(".nav-side-menu li.menu-list-item").removeClass("active");
+        $(".nav-side-menu .sub-menu.collapse li").removeClass("active");
+
+        //activating its own ul
+        $(this).addClass("active");
+        var ulListOwner = $(this).attr("data-target");
+
+        var mainListItems = $("li.menu-list-item");
+        mainListItems.each(function(idx, li) {
+            var attr = $(li).attr('data-target');
+            if (typeof attr !== typeof undefined && attr !== false && ulListOwner != attr) {
+                $(li).removeClass("collapsed");
+                $(li).attr("aria-expanded","false");
+                $(li).collapse('hide');
+                console.log("collapsing: "+li.outerHTML);
+
+            }
+        });
+        console.log("ha ha");
+    });
+});
--- js/custom/jobFinderInfoAdd.js
+++ js/custom/jobFinderInfoAdd.js
@@ -2,7 +2,9 @@
  * Created by Ramindu on 6/25/16.
  */
 var educationalInfo = [];
+var workInfo = [];
 var currentItemsInEducationalInfo = 0;
+var currentItemsInWorkInfo = 0;
 
 
 function changeFinderInformationAddView(changingView){
@@ -24,7 +26,7 @@ function addEducationalInfo(){
     var table = document.getElementById("educationInfoTable");
     if(currentItemsInEducationalInfo == 0){
         var headRow = document.createElement("tr");
-        headRow.setAttribute("id", "tableHeaderId");
+        headRow.setAttribute("id", "eduTableHeaderId");
         // headRow.innerHTML = "<th class=\"three columns\"><h5 style=\"text-align:left;float:left;\">School/University</h5></th>" +
         //                     "<th class=\"three columns\"><h5 style=\"text-align:left;float:left;\">Degree Level</h5></th>" +
         //                     "<th class=\"three columns\"><h5 style=\"text-align:left;float:left;\">Year</h5></th>" +
@@ -37,13 +39,12 @@ function addEducationalInfo(){
         table.appendChild(headRow);
     }
     currentItemsInEducationalInfo++;
-    item["schoolOrUni"] = document.getElementById('schoolOrUniversityName').value;
-    item["year"] = document.getElementById('year').value;
-    var degreeLevelSelector = document.getElementById("degreeLevel");
+    item["schoolOrUni"] = document.getElementById('school-university').value;
+    item["year"] = document.getElementById('school-uni-year').value;
+    var degreeLevelSelector = document.getElementById("degree-level");
     item["degreeLevel"] = degreeLevelSelector.options[degreeLevelSelector.selectedIndex].value;
-    item["fieldOfStudy"] = document.getElementById('fieldOfStudy').value;
-    item["grade"] = document.getElementById('grade').value;
-    item["country"] = document.getElementById('country').value;
+    item["fieldOfStudy"] = document.getElementById('field-of-study').value;
+    item["grade"] = document.getElementById('grade-s').value;
     item["key"] = "educationRow_" + currentItemsInEducationalInfo;
     
     var dataRow = document.createElement("tr");
@@ -63,7 +64,7 @@ function addEducationalInfo(){
     var a = document.createElement("a");
     a.setAttribute("href", "#");
     a.setAttribute("onclick", "return false;");
-    img.setAttribute("src", "../resources/images/common/remove_icon.png");
+    img.setAttribute("src", "img/common/remove_icon.png");
     img.setAttribute("alt", "remove icon");
     img.setAttribute("class", "button");
     img.setAttribute("style", "width:25px;height:25px;float: left;");
@@ -72,25 +73,112 @@ function addEducationalInfo(){
     dataCell.appendChild(a);
     dataRow.appendChild(dataCell);
 
-
     table.appendChild(dataRow);
 
     educationalInfo.push(item);
-    document.getElementById('schoolOrUniversityName').value = "";
-    document.getElementById('year').value = "";
+    document.getElementById('school-university').value = "";
+    document.getElementById('school-uni-year').value = "";
     $('#degreeLevel :selected').attr('selected', '');
-    document.getElementById('fieldOfStudy').value = "";
-    document.getElementById('grade').value = "";
-    document.getElementById('country').value = "";
+    document.getElementById('field-of-study').value = "";
+    document.getElementById('grade-s').value = "";
 
-    $('#educationalModal').modal('hide');
+    var container = document.getElementById("educationalInfoDiv");
+    container.style.display = 'block';
+
 }
 
 function removeEducationalInfo(currentItemId){
     currentItemsInEducationalInfo--;
     if(currentItemsInEducationalInfo == 0){
-        var el = document.getElementById('tableHeaderId');
+        var el = document.getElementById('eduTableHeaderId');
         el.parentNode.removeChild(el);
+        var container = document.getElementById("educationalInfoDiv");
+        container.style.display = 'none';
+    }
+    el = document.getElementById("educationRow_" + currentItemId);
+    el.parentNode.removeChild(el);
+
+    for(var i = 0; i < educationalInfo.length; i++) {
+        var a = educationalInfo[i];
+
+        if(a.key === "educationRow_" + currentItemId) {
+            educationalInfo.splice(i, 1);
+            break;
+        }
+    }
+}
+
+function addWorkInfo(){
+    var item = {};
+    var table = document.getElementById("workInfoTable");
+    if(currentItemsInEducationalInfo == 0){
+        var headRow = document.createElement("tr");
+        headRow.setAttribute("id", "workTableHeaderId");
+        // headRow.innerHTML = "<th class=\"three columns\"><h5 style=\"text-align:left;float:left;\">School/University</h5></th>" +
+        //                     "<th class=\"three columns\"><h5 style=\"text-align:left;float:left;\">Degree Level</h5></th>" +
+        //                     "<th class=\"three columns\"><h5 style=\"text-align:left;float:left;\">Year</h5></th>" +
+        //                     "<th class=\"three columns\"><h5 style=\"text-align:left;float:left;\">remove</h5></th>";
+
+        headRow.innerHTML = "<th>Work Place</th>" +
+            "<th>Designation</th>" +
+            "<th>No of Years</th>" +
+            "<th>remove</th>";
+        table.appendChild(headRow);
+    }
+    currentItemsInWorkInfo++;
+    item["work-place"] = document.getElementById('work-place').value;
+    item["number-of-years"] = document.getElementById('number-of-years').value;
+    item["designation"] = document.getElementById("designation").value;
+    item["work-description"] = document.getElementById('work-description').value;
+    item["key"] = "workRow_" + currentItemsInWorkInfo;
+
+    var dataRow = document.createElement("tr");
+    dataRow.setAttribute("id", "workRow_" + currentItemsInWorkInfo);
+    // dataRow.innerHTML = "<td class=\"three columns\"><h5 style=\"text-align:center;float:left;\"/>" + item["schoolOrUni"] + "</h5></td>" +
+    //                     "<td class=\"three columns\"><h5 style=\"text-align:center;float:left;\">" + item["degreeLevel"] + "</h5></td>" +
+    //                     "<td class=\"three columns\"><h5 style=\"text-align:center;float:left;\">" + item["year"] + "</h5></td>";
+
+    dataRow.innerHTML = "<td style=\"text-align:center;\">" + item["work-place"] + "</td>" +
+        "<td style=\"text-align:center;\">" + item["designation"] + "</td>" +
+        "<td style=\"text-align:center;\">" + item["number-of-years"] + "</td>";
+
+
+    var dataCell = document.createElement("td");
+    dataCell.setAttribute("style", "text-align:center");
+    var img = document.createElement("img");
+    var a = document.createElement("a");
+    a.setAttribute("href", "#");
+    a.setAttribute("onclick", "return false;");
+    img.setAttribute("src", "img/common/remove_icon.png");
+    img.setAttribute("alt", "remove icon");
+    img.setAttribute("class", "button");
+    img.setAttribute("style", "width:25px;height:25px;float: left;");
+    img.setAttribute("onclick", "removeWorkInfo("+currentItemsInWorkInfo+")");
+    a.appendChild(img);
+    dataCell.appendChild(a);
+    dataRow.appendChild(dataCell);
+
+    table.appendChild(dataRow);
+
+    workInfo.push(item);
+    document.getElementById('work-place').value = "";
+    document.getElementById('number-of-years').value = "";
+    document.getElementById('designation').value = "";
+    document.getElementById('work-description').value = "";
+
+    var container = document.getElementById("workInfoDiv");
+    container.style.display = 'block';
+
+}
+
+function removeWorkInfo(currentItemId){
+    currentItemsInEducationalInfo--;
+    if(currentItemsInEducationalInfo == 0){
+        var el = document.getElementById('workTableHeaderId');
+        el.parentNode.removeChild(el);
+
+        var container = document.getElementById("workInfoDiv");
+        container.style.display = 'none';
     }
     el = document.getElementById("educationRow_" + currentItemId);
     el.parentNode.removeChild(el);
